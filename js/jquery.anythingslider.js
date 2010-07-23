05ef8acab651b62d0faf6d31a2c08430a7731358
unknown <dj@isvonline.org>
2010-07-23 16:05:38 -0700

    CSS File renamed to anythingSlider.css Wrapper divs no longer necessary in html (object is wrapped using jquery) CSS rules now use classes created by the jquery Hash links now work for multiple panels Demo page download url redirects to github

--- js/jquery.anythingslider.js
+++ js/jquery.anythingslider.js
@@ -1,5 +1,5 @@
 /*
-	anythingSlider v1.3
+	anythingSlider v1.3.1
 	
 	By Chris Coyier: http://css-tricks.com
 	with major improvements by Doug Neiner: http://pixelgraphics.us/
@@ -20,217 +20,225 @@
 
 (function($) {
 	
+	__anythingSliderRunTimes = 0; 
+	
 	$.anythingSlider = function(el, options) {
-	  
-	// To avoid scope issues, use 'base' instead of 'this'
-	// to reference this class from internal events and functions.
-	var base = this;
 		
-	// Access to jQuery and DOM versions of element
-	base.$el = $(el);
-	base.el = el; 
-
-	// Set up a few defaults
-	base.currentPage = 1;
-	base.timer = null;
-	base.playing = false;
-
-	// Add a reverse reference to the DOM object
-	base.$el.data("AnythingSlider", base);
+		// To avoid scope issues, use 'base' instead of 'this'
+		// to reference this class from internal events and functions.
+		var base = this;
 	  
-	base.init = function() {
-		
-		base.options = $.extend({},$.anythingSlider.defaults, options);
+    	// Keeps track of the index of the current instance
+		__anythingSliderRunTimes++;
+		base.runTimes = __anythingSliderRunTimes;
 			
-		// Cache existing DOM elements for later 
-		base.$wrapper = base.$el.find('> div').css('overflow', 'hidden');
-		base.$slider  = base.$wrapper.find('> ul');
-		base.$items   = base.$slider.find('> li');
-		base.$single  = base.$items.filter(':first');
-
-		// Get the details
-		base.singleWidth = base.$single.outerWidth();
-		base.pages = base.$items.length;
-
-		// Remove navigation & player if there is only one page
-		if (base.pages === 1) {
-			base.options.autoPlay = false;
-			base.options.buildNavigation = false;
-		}
-
-		// Build the navigation if needed
-		if(base.options.buildNavigation) base.buildNavigation();
+		// Wraps the ul in the necessary divs and then gives Access to jQuery element
+		base.$el = $(el).wrap('<div class="anythingSlider"><div class="anythingWindow" /></div>').closest('div.anythingSlider');
+		
+		// Set up a few defaults
+		base.currentPage = 1;
+		base.timer = null;
+		base.playing = false;
 	
-		// Top and tail the list with 'visible' number of items, top has the last section, and tail has the first
-		// This supports the "infinite" scrolling
-		// Ensures any cloned elements with ID's have unique ID's
-		var $itemClone = base.$items.filter(':last').clone().addClass('cloned');
-		if ($itemClone.attr("id") != "") {
-		  base.$items.filter(':first').before($itemClone.attr('id', ($itemClone.attr('id') + "-cloned")));
-		} else {
-		  base.$items.filter(':first').before($itemClone);
-		}
-
-		$itemClone = base.$items.filter(':first').clone().addClass('cloned');
-		if ($itemClone.attr("id") != '') {
-		  base.$items.filter(':last' ).after($itemClone.attr('id', ($itemClone.attr('id') + "-cloned")));
-		} else {
-		  base.$items.filter(':last' ).after($itemClone);
-		}
-
-		// We just added two items, time to re-cache the list
-		base.$items = base.$slider.find('> li'); // reselect
-			
-		// Setup our forward/backward navigation - if more than one page exists
-		if (base.pages > 1) {
-		  base.buildNextBackButtons();
-		}
-		
-		// If autoPlay functionality is included, then initialize the settings
-		if (base.options.autoPlay) {
-			base.playing = !base.options.startStopped; // Sets the playing variable to false if startStopped is true
-			base.buildAutoPlay();
-		};
-		
-		// If pauseOnHover then add hover effects
-		if (base.options.pauseOnHover) {
-			base.$el.hover(function() {
-				base.clearTimer();
-			}, function() {
-				base.startStop(base.playing);
-			});
-		}
-		
-		// If a hash can not be used to trigger the plugin, then go to page 1
-		if ((base.options.hashTags == true && !base.gotoHash()) || base.options.hashTags == false) {
-			base.setCurrentPage(1);
-		};
-	}
-
-	base.gotoPage = function(page, autoplay) {
-		// When autoplay isn't passed, we stop the timer
-		if (autoplay !== true) autoplay = false;
-		if (!autoplay) base.startStop(false);
-		
-		if (typeof(page) == "undefined" || page == null) {
-			page = 1;
-			base.setCurrentPage(1);
-		};
-		
-		// Stop the slider when we reach the last page, if the option stopAtEnd is set to true
-		if(base.options.stopAtEnd){
-			if(page == base.pages) base.startStop(false);
-		}
-		
-		// Just check for bounds
-		if (page > base.pages + 1) page = base.pages;
-		if (page < 0 ) page = 1;
-
-		var dir = page < base.currentPage ? -1 : 1,
-		n = Math.abs(base.currentPage - page),
-		left = base.singleWidth * dir * n;
-		
-		base.$wrapper.filter(':not(:animated)').animate({
-			scrollLeft : '+=' + left
-		}, base.options.animationTime, base.options.easing, function () {
-	   
-			if (page == 0) {
-				base.$wrapper.scrollLeft(base.singleWidth * base.pages);
-				page = base.pages;
-			} else if (page > base.pages) {
-				base.$wrapper.scrollLeft(base.singleWidth);
-				// reset back to start position
-				page = 1;
+		// Add a reverse reference to the DOM object
+		base.$el.data("AnythingSlider", base);
+		  
+		base.init = function() {
+		 			
+			base.options = $.extend({},$.anythingSlider.defaults, options);
+				
+			// Cache existing DOM elements for later 
+			base.$wrapper = base.$el.find('> div').css('overflow', 'hidden');
+			base.$slider  = base.$wrapper.find('> ul');
+			base.$items   = base.$slider.find('> li');
+			base.$single  = base.$items.filter(':first');
+	
+			// Get the details
+			base.singleWidth = base.$single.outerWidth();
+			base.pages = base.$items.length;
+	
+			// Remove navigation & player if there is only one page
+			if (base.pages === 1) {
+				base.options.autoPlay = false;
+				base.options.buildNavigation = false;
 			}
-  
-			base.setCurrentPage(page);
-		});
-	};
-		
-	base.setCurrentPage = function(page, move) {
-		// Set visual
-		if (base.options.buildNavigation){
-			base.$nav.find('.cur').removeClass('cur');
-			$(base.$navLinks[page - 1]).addClass('cur');
-		};
-		
-		// Only change left if move does not equal false
-		if (move !== false) base.$wrapper.scrollLeft(base.singleWidth * page);
-
-		// Update local variable
-		base.currentPage = page;
-	};
 	
-	base.goForward = function(autoplay) {
-		if(autoplay !== true) autoplay = false;
-		base.gotoPage(base.currentPage + 1, autoplay);
-	};
+			// Build the navigation if needed
+			if(base.options.buildNavigation) base.buildNavigation();
+		
+			// Top and tail the list with 'visible' number of items, top has the last section, and tail has the first
+			// This supports the "infinite" scrolling
+			// Ensures any cloned elements with ID's have unique ID's
+			var $itemClone = base.$items.filter(':last').clone().addClass('cloned');
+			if ($itemClone.attr("id") != "") {
+			  base.$items.filter(':first').before($itemClone.attr('id', ($itemClone.attr('id') + "-cloned")));
+			} else {
+			  base.$items.filter(':first').before($itemClone);
+			}
 	
-	base.goBack = function() {
-		base.gotoPage(base.currentPage - 1);
-	};
-		
-	// This method tries to find a hash that matches panel-X
-	// If found, it tries to find a matching item
-	// If that is found as well, then that item starts visible
-	base.gotoHash = function(){
-		if (/^#?panel-\d+$/.test(window.location.hash)) {
-			var index = parseInt(window.location.hash.substr(7));
-			var $item = base.$items.filter(':eq(' + index + ')');
-			if ($item.length != 0) {
-				base.setCurrentPage(index);
-				return true;
-			};
-		};
-		return false; // A item wasn't found;
-	};
-		
-	// Creates the numbered navigation links
-	base.buildNavigation = function() {
-		base.$nav = $("<div class='thumbNav'><ul/></div>").prependTo(base.$el);
-		
-		if (base.options.buildNavigation && (base.pages > 1)) {
-			base.$items.each(function(i,el) {
-				var index = i + 1;
-				var $a = $("<a href='#'></a>");
+			$itemClone = base.$items.filter(':first').clone().addClass('cloned');
+			if ($itemClone.attr("id") != '') {
+			  base.$items.filter(':last' ).after($itemClone.attr('id', ($itemClone.attr('id') + "-cloned")));
+			} else {
+			  base.$items.filter(':last' ).after($itemClone);
+			}
+	
+			// We just added two items, time to re-cache the list
+			base.$items = base.$slider.find('> li'); // reselect
 				
-				// If a formatter function is present, use it
-				if (typeof(base.options.navigationFormatter) == "function") {
-					$a.html(base.options.navigationFormatter(index, $(this)));
-				} else {
-					$a.text(index);
-				}
-				
-				$a.click(function(e) {
-					base.gotoPage(index);
-					if (base.options.hashTags) base.setHash('panel-' + index);
-					e.preventDefault();
-				});
-				
-				$("ul", base.$nav).append($a);
-				$a.wrap("<li />");
-			});
-			base.$navLinks = base.$nav.find('li > a');
+			// Setup our forward/backward navigation - if more than one page exists
+			if (base.pages > 1) {
+			  base.buildNextBackButtons();
+			}
 			
+			// If autoPlay functionality is included, then initialize the settings
+			if (base.options.autoPlay) {
+				base.playing = !base.options.startStopped; // Sets the playing variable to false if startStopped is true
+				base.buildAutoPlay();
+			};
+			
+			// If pauseOnHover then add hover effects
+			if (base.options.pauseOnHover) {
+				base.$el.hover(function() {
+					base.clearTimer();
+				}, function() {
+					base.startStop(base.playing);
+				});
+			}
+			
+			// If a hash can not be used to trigger the plugin, then go to page 1
+			if ((base.options.hashTags == true && !base.gotoHash()) || base.options.hashTags == false) {
+				base.setCurrentPage(1);
+			};
 		}
-	};
+	
+		base.gotoPage = function(page, autoplay) {
+			// When autoplay isn't passed, we stop the timer
+			if (autoplay !== true) autoplay = false;
+			if (!autoplay) base.startStop(false);
+			
+			if (typeof(page) == "undefined" || page == null) {
+				page = 1;
+				base.setCurrentPage(1);
+			};
+			
+			// Stop the slider when we reach the last page, if the option stopAtEnd is set to true
+			if(base.options.stopAtEnd){
+				if(page == base.pages) base.startStop(false);
+			}
+			
+			// Just check for bounds
+			if (page > base.pages + 1) page = base.pages;
+			if (page < 0 ) page = 1;
+	
+			var dir = page < base.currentPage ? -1 : 1,
+			n = Math.abs(base.currentPage - page),
+			left = base.singleWidth * dir * n;
+			
+			base.$wrapper.filter(':not(:animated)').animate({
+				scrollLeft : '+=' + left
+			}, base.options.animationTime, base.options.easing, function () {
+				if (page == 0) {
+					base.$wrapper.scrollLeft(base.singleWidth * base.pages);
+					page = base.pages;
+				} else if (page > base.pages) {
+					base.$wrapper.scrollLeft(base.singleWidth);
+					// reset back to start position
+					page = 1;
+				}
+	  
+				base.setCurrentPage(page);
+			});
+		};
+			
+		base.setCurrentPage = function(page, move) {
+			// Set visual
+			if (base.options.buildNavigation){
+				base.$nav.find('.cur').removeClass('cur');
+				$(base.$navLinks[page - 1]).addClass('cur');
+			};
+			
+			// Only change left if move does not equal false
+			if (move !== false) base.$wrapper.scrollLeft(base.singleWidth * page);
+	
+			// Update local variable
+			base.currentPage = page;
+		};
 		
-	// Creates the Forward/Backward buttons
-	base.buildNextBackButtons = function() {
-		var $forward = $('<li class="arrow forward"><a href="#">' + base.options.forwardText + '</a></li>'),
-			$back    = $('<li class="arrow back"><a href="#">' + base.options.backText + '</a></li>');
+		base.goForward = function(autoplay) {
+			if(autoplay !== true) autoplay = false;
+			base.gotoPage(base.currentPage + 1, autoplay);
+		};
+		
+		base.goBack = function() {
+			base.gotoPage(base.currentPage - 1);
+		};
+			
+		// This method tries to find a hash that matches panel-X
+		// If found, it tries to find a matching item
+		// If that is found as well, then that item starts visible
+		base.gotoHash = function(){
+			var hash = window.location.hash.match(/^#?panel(\d+)-(\d+)$/);
+			if (hash) {
+				var panel = parseInt(hash[1]);
+				if (panel == base.runTimes) {
+					var slide = parseInt(hash[2]);
+					var $item = base.$items.filter(':eq(' + slide + ')');
+					if ($item.length != 0) {
+						base.setCurrentPage(slide);
+						return true;
+					}
+				}
+			}
+			return false; // A item wasn't found;
+		};
+			
+		// Creates the numbered navigation links
+		base.buildNavigation = function() {
+			base.$nav = $("<div class='thumbNav'><ul/></div>").prependTo(base.$el);
+			
+			if (base.options.buildNavigation && (base.pages > 1)) {
+				base.$items.each(function(i,el) {
+					var index = i + 1;
+					var $a = $("<a href='#'></a>");
+					
+					// If a formatter function is present, use it
+					if (typeof(base.options.navigationFormatter) == "function") {
+						$a.html(base.options.navigationFormatter(index, $(this)));
+					} else {
+						$a.text(index);
+					}
+					
+					$a.click(function(e) {
+						base.gotoPage(index);
+						if (base.options.hashTags) base.setHash('panel' + base.runTimes + '-' + index);
+						e.preventDefault();
+					});
+					
+					$("ul", base.$nav).append($a);
+					$a.wrap("<li />");
+				});
+				base.$navLinks = base.$nav.find('li > a');
 				
-		// Bind to the forward and back buttons
-		$back.click(function(e) {
-			base.goBack();
-			e.preventDefault();
-		});
-		$forward.click(function(e) {
-			base.goForward();
-			e.preventDefault();
-		});
-
-		// Append elements to page
+			}
+		};
+		
+		// Creates the Forward/Backward buttons
+		base.buildNextBackButtons = function() {
+			var $forward = $('<li class="arrow forward"><a href="#">' + base.options.forwardText + '</a></li>'),
+				$back    = $('<li class="arrow back"><a href="#">' + base.options.backText + '</a></li>');
+					
+			// Bind to the forward and back buttons
+			$back.click(function(e) {
+				base.goBack();
+				e.preventDefault();
+			});
+			$forward.click(function(e) {
+				base.goForward();
+				e.preventDefault();
+			});
+	
+			// Append elements to page
 			$("ul", base.$nav).prepend($back).append($forward);
 		};
 		
@@ -315,10 +323,6 @@
 		if (typeof(options) == "object"){
 			return this.each(function(i){			
 				(new $.anythingSlider(this, options));
-			
-				// This plugin supports multiple instances, but only one can support hash-tag support
-				// This disables hash-tags on all items but the first one
-				options.hashTags = false;
 			});	
 		
 		} else if (typeof(options) == "number") {
