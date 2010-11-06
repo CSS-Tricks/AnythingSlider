7e6e4ad4ac1dcf8721a4e82b08131d9d4f0970d7
Rob Garrison <wowmotty@gmail.com>
2010-11-06 17:28:30 -0500

    fx demo - colorbox now shows image gallery

--- js/demo.js
+++ js/demo.js
@@ -14,19 +14,19 @@ $(document).ready(function(){
 		toggleControls : true,
 		theme          : 'metallic',
 		navigationFormatter : function(i, panel){
-			return '<img src="images/th-slide-' + ['civil-1', 'env-1', 'civil-2', 'env-2'][i - 1] + '.jpg">';
+			return '<img src="images/th-slide-' + ['civil-1', 'env-1', 'civil-2', 'env-2'][i-1] + '.jpg">';
 		}
 	})
-	.find('img').click(function(){
-		$.colorbox({
+	.find('.panel:not(.cloned) img') // ignore the cloned panels
+		.attr('rel','group')           // add all slider images to a colorbox group
+		.colorbox({
 			width: '90%',
 			height: '90%',
-			scalePhotos : true,
-			href: $(this).attr('src'),
+			href: function(){ return $(this).attr('src'); },
 			// use $(this).attr('title') for specific image captions
-			title: 'Press escape to close'
+			title: 'Press escape to close',
+			rel: 'group'
 		});
-	});
 
 	$('#slider2')
 	.anythingSlider({
